9a2cfb4d7470aeaef971148499028a59082a1681
jenna-daly <jenna.daly1@marist.edu>
2017-02-21 23:42:23 -0500

    Jenna Daly lab3
    Jenna Daly lab3

--- labs/Lab3/Driver_lab3.java
+++ labs/Lab3/Driver_lab3.java
@@ -21,16 +21,20 @@ import java.util.Scanner;
 public class Driver_lab3 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-
     double x1 = input.nextDouble();
-
-    //while(x1 != 0){
+    //forms a loop that calculates p norm and stops if 0 is entered
+    while(x1 != 0){
+    //assign computer input coordinates and p value to variables
     double y1 = input.nextDouble();
     double x2 = input.nextDouble();
     double y2 = input.nextDouble();
     double p = input.nextDouble();
-
+    //print out p norm
     System.out.printf("%.10f", Math.pow(Math.pow(Math.abs(x1-x2), p) + Math.pow(Math.abs(y1-y2), p), 1/p));
-    //}
+    System.out.println("");
+    //starts new x1
+    x1 = input.nextDouble();
+    }
+    return;
   }
 }
